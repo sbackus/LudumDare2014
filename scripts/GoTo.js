a84806362b5639890ff818f95cadbd0334af80fb
Sam Backus <Samuel.Backus@gmail.com>
2014-12-06 19:52:12 -0500

    proof of concept done

--- scripts/GoTo.js
+++ scripts/GoTo.js
@@ -1,9 +1,17 @@
-$( document ).ready(function() {
-  function goTo(tag_id) {
-    alert("this got clicked!")
-    var e = document.getElementById(tag_id)
-    alert(e.innerHTML)
-    e.click()
-  }
+$(document).ready(function() {
+    $("#linkToKitchen").click(function(evt) {
+        $("#Kitchen").zoomTo({targetsize:0.75, duration:600});
+        evt.stopPropagation();
+    });
+    $("#linkToBathroom").click(function(evt) {
+        $("#Bathroom").zoomTo({targetsize:0.75, duration:600});
+        evt.stopPropagation();
+    });
+    $("#linkToOutside").click(function(evt) {
+        $("#Outside").zoomTo({targetsize:0.75, duration:600});
+        evt.stopPropagation();
+    });
+
+$("#WakeUp").zoomTo({targetsize:0.75, duration:600});
 });
 
